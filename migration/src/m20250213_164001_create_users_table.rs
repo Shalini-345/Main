5957853915525c3f52043a0a1d238c1a1922e983
Shalinis7-works <shalini@s7works.io>
2025-02-13 22:36:14 +0530

    changes

--- migration/src/m20250213_164001_create_users_table.rs
+++ migration/src/m20250213_164001_create_users_table.rs
@@ -6,26 +6,20 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
             .create_table(
                 Table::create()
                     .table(Post::Table)
                     .if_not_exists()
-                    .col(pk_auto(Post::Id))
-                    .col(string(Post::Title))
-                    .col(string(Post::Text))
+                    .col(pk_auto(Post::Id)) // Primary Key, auto-increment
+                    .col(string(Post::Title)) // Title of the post
+                    .col(string(Post::Text)) // Text content of the post
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
             .drop_table(Table::drop().table(Post::Table).to_owned())
             .await
