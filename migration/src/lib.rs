7cba75b58c4aa9a864e875979d64fc2095949677
Ubuntu <ubuntu@ip-172-31-41-209.eu-north-1.compute.internal>
2025-02-13 16:46:37 +0000

    changes

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -2,6 +2,7 @@ pub use sea_orm_migration::prelude::*;
 
 mod m20220101_000001_create_table;
 mod m20250213_073143_create_drivers_table;
+mod m20250213_164001_create_users_table;
 
 pub struct Migrator;
 
@@ -11,6 +12,7 @@ impl MigratorTrait for Migrator {
         vec![
             Box::new(m20220101_000001_create_table::Migration),
             Box::new(m20250213_073143_create_drivers_table::Migration),
+            Box::new(m20250213_164001_create_users_table::Migration),
         ]
     }
 }
