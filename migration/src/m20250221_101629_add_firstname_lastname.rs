835cdbc8c1d09d3d7f533b256f8ab09d019d28cb
Shalinis7-works <shalini@s7works.io>
2025-02-21 17:18:18 +0530

    changes committed

--- migration/src/m20250221_101629_add_firstname_lastname.rs
+++ migration/src/m20250221_101629_add_firstname_lastname.rs
@@ -1,4 +1,5 @@
 use sea_orm_migration::prelude::*;
+use sea_orm::DbErr;
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -6,15 +7,31 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager
-            .alter_table(
-                Table::alter()
-                    .table(Users::Table)
-                    .add_column(ColumnDef::new(Users::FirstName).string().not_null())
-                    .add_column(ColumnDef::new(Users::LastName).string().not_null())
-                    .to_owned(),
-            )
-            .await
+        // Check if first_name column exists, if not, add it
+        if !manager.has_column(Users::Table.as_ref(), Users::FirstName.as_ref()).await? {
+            manager
+                .alter_table(
+                    Table::alter()
+                        .table(Users::Table)
+                        .add_column(ColumnDef::new(Users::FirstName).string().not_null())
+                        .to_owned(),
+                )
+                .await?;
+        }
+
+        // Check if last_name column exists, if not, add it
+        if !manager.has_column(Users::Table.as_ref(), Users::LastName.as_ref()).await? {
+            manager
+                .alter_table(
+                    Table::alter()
+                        .table(Users::Table)
+                        .add_column(ColumnDef::new(Users::LastName).string().not_null())
+                        .to_owned(),
+                )
+                .await?;
+        }
+
+        Ok(())
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
@@ -35,4 +52,14 @@ enum Users {
     Table,
     FirstName,
     LastName,
+}
+
+impl AsRef<str> for Users {
+    fn as_ref(&self) -> &str {
+        match self {
+            Users::Table => "users",
+            Users::FirstName => "first_name",
+            Users::LastName => "last_name",
+        }
+    }
 }
